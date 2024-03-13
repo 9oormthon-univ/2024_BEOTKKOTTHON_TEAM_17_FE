d0d92d4d9cd24ce89aba7948da107e0fa0bbb563
pookey1104 <pookey1104@naver.com>
2024-03-13 17:04:50 +0900

    FEAT : 메인 세팅 재구성 & 이미지 일부 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,40 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import "../styles/Main.css";
 
 import MainHeader from "../components/MainHeader";
 
-const Page = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  overflow-y: scroll;
-  overflow-x: hidden;
-`;
-
 const MainPage = styled.div`
   background-image: linear-gradient(#138eff, #006eee);
-  width: 100vw;
-  min-height: 100vh;
-
-  @media (hover: hover) and (pointer: fine) {
-    width: 375px;
-  }
-`;
-
-const FlexCenter = styled.div`
-  display: flex;
-  justify-content: center;
 `;
 
 function Main() {
   return (
-    <Page>
-      <FlexCenter>
+    <div className="page">
+      <div className="center">
         <MainPage>
-          <MainHeader />
-          <p>메인페이지</p>
+          <div className="page-space">
+            <MainHeader />
+            <p>메인페이지</p>
+          </div>
         </MainPage>
-      </FlexCenter>
-    </Page>
+      </div>
+    </div>
   );
 }
 
