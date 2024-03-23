694c3c1b786860fef4fa810be7ce53e869b47a30
Songminseok <songms0909@khu.ac.kr>
2024-03-23 14:30:44 +0900

    Feat : 카테고리 추가 로직 변경

--- src/pages/MyCardsCategory.js
+++ src/pages/MyCardsCategory.js
@@ -6,25 +6,24 @@ import { getCategoryList, createCategory, deleteCategory } from "../utils/axios"
 
 const MyCardsCategory = ({ onToggle }) => {
   const navigate = useNavigate();
-  const [newCategoryItem, setNewCategoryItem] = useState(null);
+  const [showNewCategoryInput, setShowNewCategoryInput] = useState(false);
   const [categoryName, setCategoryName] = useState("");
   const [categoryList, setCategoryList] = useState([]);
   const [cookies] = useCookies();
   const token = cookies["jwt-token"];
 
   const handleToListLink = () => {
-    onToggle(); // 클릭 이벤트 발생 시 onToggle 함수 호출
+    onToggle();
   };
 
   const fetchCategoryList = async () => {
     try {
       if (!token) {
         navigate("/");
+        return;
       }
       const response = await getCategoryList(token);
-      const categoryData = response.data;
-      setCategoryList(categoryData);
-      console.log(categoryData);
+      setCategoryList(response.data);
     } catch (error) {
       console.error("Error fetching category list:", error);
     }
@@ -32,11 +31,10 @@ const MyCardsCategory = ({ onToggle }) => {
 
   useEffect(() => {
     fetchCategoryList();
-  }, []);
+  }, [token]);
 
   const handleToCategoryCard = (category) => {
     navigate(`/mycards/category/${category.categoryId}`, { state: { category } });
-    console.log(category);
   };
 
   const handleChangeInputData = (e) => {
@@ -50,73 +48,22 @@ const MyCardsCategory = ({ onToggle }) => {
   };
 
   const handleCategoryBtnClick = () => {
-    const newItem = (
-      <CategoryList key="new-category">
-        <CategoryListLeft>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="18" viewBox="0 0 25 18" fill="none">
-            <g clip-path="url(#clip0_1_6988)">
-              <path
-                d="M20.7693 0H3.22945C1.44587 0 0 1.44052 0 3.2175V14.7825C0 16.5595 1.44587 18 3.22945 18H20.7693C22.5529 18 23.9988 16.5595 23.9988 14.7825V3.2175C23.9988 1.44052 22.5529 0 20.7693 0Z"
-                fill="#138EFF"
-              />
-              <path
-                d="M19.9714 6.105H24.059C24.3301 6.105 24.5484 6.3225 24.5484 6.5925V11.4075C24.5484 11.6775 24.3301 11.895 24.059 11.895H19.9714C18.368 11.895 17.0657 10.5975 17.0657 9C17.0657 7.4025 18.368 6.105 19.9714 6.105Z"
-                fill="#138EFF"
-                stroke="white"
-                stroke-width="1.2"
-                stroke-miterlimit="10"
-              />
-              <path
-                d="M19.6327 9.9375C20.1524 9.9375 20.5737 9.51777 20.5737 9C20.5737 8.48223 20.1524 8.0625 19.6327 8.0625C19.113 8.0625 18.6917 8.48223 18.6917 9C18.6917 9.51777 19.113 9.9375 19.6327 9.9375Z"
-                fill="white"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_1_6988">
-                <rect width="25" height="18" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-          <CategoryInput
-            type="text"
-            value={categoryName}
-            onChange={handleChangeInputData}
-            onKeyPress={handleOnKeyPress}
-            placeholder="정보를 입력하세요."
-          />
-        </CategoryListLeft>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="17"
-          height="17"
-          viewBox="0 0 17 17"
-          fill="none"
-          cursor="pointer"
-          onClick={createCategoryList}
-        >
-          <path
-            d="M11.2327 2.59385L14.4236 5.74987M14.7619 1.50552L15.4945 2.23817C16.1685 2.91219 16.1685 4.00499 15.4945 4.67901L5.79833 14.3751C5.60887 14.5646 5.3779 14.7073 5.12371 14.7921L1.56868 15.9771C1.23136 16.0896 0.910451 15.7686 1.02289 15.4313L2.2079 11.8763C2.29263 11.6221 2.43538 11.3911 2.62485 11.2016L12.321 1.50551C12.995 0.831496 14.0878 0.831496 14.7619 1.50552Z"
-            stroke="#8C8C8C"
-            stroke-width="1.2"
-            stroke-linecap="round"
-          />
-          <path d="M2.47559 11.0722L5.92746 14.5241" stroke="#8C8C8C" stroke-width="1.2" />
-        </svg>
-      </CategoryList>
-    );
-    setNewCategoryItem(newItem);
+    setShowNewCategoryInput(true);
   };
 
   const createCategoryList = async () => {
     try {
       if (!token) {
         navigate("/");
+        return;
       }
-      const response = await createCategory(categoryName, token);
-      setCategoryName(""); // 입력값 초기화
+      await createCategory(categoryName, token);
+      setCategoryName("");
+
       fetchCategoryList();
+      setShowNewCategoryInput(false);
     } catch (error) {
-      console.log(error);
+      console.error("Error creating category:", error);
       navigate("/");
     }
   };
@@ -125,11 +72,12 @@ const MyCardsCategory = ({ onToggle }) => {
     try {
       if (!token) {
         navigate("/");
+        return;
       }
-      const response = await deleteCategory(category.categoryId, token);
+      await deleteCategory(category.categoryId, token);
       fetchCategoryList();
     } catch (error) {
-      console.log(error);
+      console.error("Error deleting category:", error);
       navigate("/");
     }
   };
@@ -145,7 +93,13 @@ const MyCardsCategory = ({ onToggle }) => {
           {categoryList.map((category) => (
             <CategoryList key={category.categoryId}>
               <CategoryListLeft onClick={() => handleToCategoryCard(category)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="18" viewBox="0 0 25 18" fill="none">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="18"
+                  viewBox="0 0 25 18"
+                  fill="none"
+                >
                   <g clip-path="url(#clip0_1_6988)">
                     <path
                       d="M20.7693 0H3.22945C1.44587 0 0 1.44052 0 3.2175V14.7825C0 16.5595 1.44587 18 3.22945 18H20.7693C22.5529 18 23.9988 16.5595 23.9988 14.7825V3.2175C23.9988 1.44052 22.5529 0 20.7693 0Z"
@@ -165,7 +119,11 @@ const MyCardsCategory = ({ onToggle }) => {
                   </g>
                   <defs>
                     <clipPath id="clip0_1_6988">
-                      <rect width="25" height="18" fill="white" />
+                      <rect
+                        width="25"
+                        height="18"
+                        fill="white"
+                      />
                     </clipPath>
                   </defs>
                 </svg>
@@ -186,7 +144,11 @@ const MyCardsCategory = ({ onToggle }) => {
                     stroke-width="1.2"
                     stroke-linecap="round"
                   />
-                  <path d="M2.47559 11.0722L5.92746 14.5241" stroke="#8C8C8C" stroke-width="1.2" />
+                  <path
+                    d="M2.47559 11.0722L5.92746 14.5241"
+                    stroke="#8C8C8C"
+                    stroke-width="1.2"
+                  />
                 </svg>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -208,15 +170,44 @@ const MyCardsCategory = ({ onToggle }) => {
               </CategoryListRight>
             </CategoryList>
           ))}
-          {newCategoryItem && newCategoryItem}
+          {showNewCategoryInput && (
+            <CategoryList>
+              <CategoryListLeft>
+                <CategoryInput
+                  type="text"
+                  value={categoryName}
+                  onChange={handleChangeInputData}
+                  onKeyPress={handleOnKeyPress}
+                  placeholder="정보를 입력하세요."
+                />
+              </CategoryListLeft>
+              {/* Possibly other UI elements for creating a category */}
+            </CategoryList>
+          )}
           <CategoryBtn onClick={handleCategoryBtnClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" viewBox="0 0 65 65" fill="none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="65"
+              height="65"
+              viewBox="0 0 65 65"
+              fill="none"
+            >
               <path
                 d="M65 32.5C65 50.4493 50.4493 65 32.5 65C14.5507 65 0 50.4493 0 32.5C0 14.5507 14.5507 0 32.5 0C50.4493 0 65 14.5507 65 32.5Z"
                 fill="url(#paint0_linear_51_3214)"
               />
-              <path d="M33 20V46" stroke="white" stroke-width="2.5" stroke-linecap="round" />
-              <path d="M46 33L20 33" stroke="white" stroke-width="2.5" stroke-linecap="round" />
+              <path
+                d="M33 20V46"
+                stroke="white"
+                stroke-width="2.5"
+                stroke-linecap="round"
+              />
+              <path
+                d="M46 33L20 33"
+                stroke="white"
+                stroke-width="2.5"
+                stroke-linecap="round"
+              />
               <defs>
                 <linearGradient
                   id="paint0_linear_51_3214"
@@ -226,9 +217,18 @@ const MyCardsCategory = ({ onToggle }) => {
                   y2="82"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop offset="0.0475677" stop-color="#92CBFF" />
-                  <stop offset="0.462568" stop-color="#0587FF" />
-                  <stop offset="0.752212" stop-color="#0076FF" />
+                  <stop
+                    offset="0.0475677"
+                    stop-color="#92CBFF"
+                  />
+                  <stop
+                    offset="0.462568"
+                    stop-color="#0587FF"
+                  />
+                  <stop
+                    offset="0.752212"
+                    stop-color="#0076FF"
+                  />
                 </linearGradient>
               </defs>
             </svg>
@@ -243,13 +243,24 @@ const MyCardsCategory = ({ onToggle }) => {
             viewBox="0 0 65 65"
             fill="none"
             style={{ cursor: "pointer" }}
+            onClick={() => {}}
           >
             <path
               d="M65 32.5C65 50.4493 50.4493 65 32.5 65C14.5507 65 0 50.4493 0 32.5C0 14.5507 14.5507 0 32.5 0C50.4493 0 65 14.5507 65 32.5Z"
               fill="url(#paint0_linear_51_3203)"
             />
-            <path d="M33 20V46" stroke="white" stroke-width="2.5" stroke-linecap="round" />
-            <path d="M46 33L20 33" stroke="white" stroke-width="2.5" stroke-linecap="round" />
+            <path
+              d="M33 20V46"
+              stroke="white"
+              stroke-width="2.5"
+              stroke-linecap="round"
+            />
+            <path
+              d="M46 33L20 33"
+              stroke="white"
+              stroke-width="2.5"
+              stroke-linecap="round"
+            />
             <defs>
               <linearGradient
                 id="paint0_linear_51_3203"
@@ -259,9 +270,18 @@ const MyCardsCategory = ({ onToggle }) => {
                 y2="82"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop offset="0.0475677" stop-color="#92CBFF" />
-                <stop offset="0.462568" stop-color="#0587FF" />
-                <stop offset="0.752212" stop-color="#0076FF" />
+                <stop
+                  offset="0.0475677"
+                  stop-color="#92CBFF"
+                />
+                <stop
+                  offset="0.462568"
+                  stop-color="#0587FF"
+                />
+                <stop
+                  offset="0.752212"
+                  stop-color="#0076FF"
+                />
               </linearGradient>
             </defs>
           </svg>
